3f47f4379bc556b424a12e83474240faa65589aa
Zhong Jianxin <azuwis@gmail.com>
2013-04-27 16:47:43 +0800

    delete searchbox in sidebar, add sourcelink and custom download

--- source/conf.py
+++ source/conf.py
@@ -135,7 +135,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
-    '**': ['globaltoc.html', 'searchbox.html']
+    '**': ['globaltoc.html', 'sourcelink.html', 'download.html']
 }
 
 # Additional templates that should be rendered to pages, maps page names to
